227f0d0c3c8808c5e1fd64c4727292d236c37ee8
Melih Elibol <elibol@gmail.com>
2020-10-07 20:37:58 -0700

    Enable s3 tests.

--- tests/storage/test_s3.py
+++ tests/storage/test_s3.py
@@ -6,7 +6,6 @@ from nums.core.array.blockarray import BlockArray
 from nums.core.storage.storage import StoredArrayS3
 
 
-@pytest.mark.skip
 def test_rwd(app_inst: ArrayApplication):
     array: np.ndarray = np.random.random(35).reshape(7, 5)
     ba: BlockArray = app_inst.array(array, block_shape=(3, 4))
