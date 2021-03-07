61e83c9b866e38da4e4c5ad5f4ebdf31d3dcfe9b
Daniel Zou <zoudl2000@gmail.com>
2021-03-06 20:00:26 -0600

    Fixed theme, autodoc still broken

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,9 +35,6 @@ extensions = [
     'sphinx_rtd_theme'
 ]
 
-# Theme
-html_theme = "sphinx_rtd_theme"
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -52,7 +49,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
