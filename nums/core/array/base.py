7cb58348fc1db7ca3fa6e48db5838a60ad63665d
Melih Elibol <elibol@gmail.com>
2021-03-26 16:02:35 -0700

    fixes immediate get after trans. (#31)

--- nums/core/array/base.py
+++ nums/core/array/base.py
@@ -396,18 +396,21 @@ class BlockArrayBase(object):
     def __repr__(self):
         return "BlockArray(" + str(self.blocks) + ")"
 
-    def get(self):
-        result = np.zeros(shape=self.grid.shape, dtype=self.grid.dtype)
-        block_shape = np.array(self.grid.block_shape, dtype=np.int)
-        blocks = self.system.get([self.blocks[grid_entry].oid
-                                  for grid_entry in self.grid.get_entry_iterator()])
+    def get(self) -> np.ndarray:
+        result: np.ndarray = np.zeros(shape=self.grid.shape, dtype=self.grid.dtype)
+        block_shape: np.ndarray = np.array(self.grid.block_shape, dtype=np.int)
+        arrays: list = self.system.get([self.blocks[grid_entry].oid
+                                        for grid_entry in self.grid.get_entry_iterator()])
         for block_index, grid_entry in enumerate(self.grid.get_entry_iterator()):
             start = block_shape * grid_entry
             entry_shape = np.array(self.grid.get_block_shape(grid_entry), dtype=np.int)
             end = start + entry_shape
             slices = tuple(map(lambda item: slice(*item), zip(*(start, end))))
-            block = self.blocks[grid_entry]
-            result[slices] = blocks[block_index].reshape(block.shape)
+            block: Block = self.blocks[grid_entry]
+            arr: np.ndarray = arrays[block_index]
+            if block.transposed:
+                arr = arr.T
+            result[slices] = arr.reshape(block.shape)
         return result
 
     def broadcast_to(self, shape):
