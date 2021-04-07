move broadcast checks to utils.py, fixed test coverage, fixed minor errors

nums/core/array/utils.py
@@ -21,6 +21,7 @@ import numpy as np
 import scipy.special
 from nums.core.settings import np_ufunc_map
 
+
 # pylint: disable = no-member
 
 
@@ -185,7 +186,7 @@ def get_slices(total_size, batch_size, order, reverse_blocks=False):
             # If reverse order blocks are not multiples of axis dimension,
             # then the last block is smaller than block size and should be
             # the first block.
-            result = list(reversed(list(range(-total_size-1, -1, batch_size)) + [-1]))
+            result = list(reversed(list(range(-total_size - 1, -1, batch_size)) + [-1]))
         else:
             result = list(range(-1, -total_size - 1, -batch_size)) + [-total_size - 1]
         return list(map(lambda s: slice(*s, order), zip(*(result[:-1], result[1:]))))
@@ -236,7 +237,7 @@ def addr2idx(addr: int, shape: tuple):
     val = addr
     for i in range(len(strides)):
         stride = strides[i]
-        axis_index = int(val/stride)
+        axis_index = int(val / stride)
         index.append(axis_index)
         val %= stride
     return tuple(index)
@@ -259,3 +260,46 @@ def translate_index_list(from_index_list, from_shape, to_shape):
         addr = idx2addr(src_index, from_shape)
         to_index_list.append(addr2idx(addr, to_shape))
     return to_index_list
+
+
+def np_tensordot_param_test(as_, nda, bs, ndb, axes):
+    # Error checking before everything gets passed into BlockArray operations. Modified from the
+    # original NumPy tensordot method for error checking:
+    # https://github.com/numpy/numpy/blob/v1.20.0/numpy/core/numeric.py#L949-L1139
+
+    try:
+        iter(axes)
+    except Exception:
+        axes_a = list(range(-axes, 0))
+        axes_b = list(range(0, axes))
+    else:
+        axes_a, axes_b = axes
+    try:
+        na = len(axes_a)
+        axes_a = list(axes_a)
+    except TypeError:
+        axes_a = [axes_a]
+        na = 1
+    try:
+        nb = len(axes_b)
+        axes_b = list(axes_b)
+    except TypeError:
+        axes_b = [axes_b]
+        nb = 1
+
+    equal = True
+    if na != nb:
+        equal = False
+
+    else:
+        for k in range(na):
+            if as_[axes_a[k]] != bs[axes_b[k]]:
+                equal = False
+                break
+            if axes_a[k] < 0:
+                axes_a[k] += nda
+            if axes_b[k] < 0:
+                axes_b[k] += ndb
+
+    if not equal:
+        raise ValueError("shape-mismatch for sum")

tests/numpy/test_arithmetic.py
@@ -123,14 +123,14 @@ def test_matmul_tensor(nps_app_inst):
 
     assert nps_app_inst is not None
 
-    # TODO(bcp): Replace with matmul tests for rank > 2 once implemented.
-    def check_matmul_tensor(_ns_a, _ns_b):
+    # TODO (bcp): Replace with matmul tests for rank > 2 once implemented.
+    def test_matmul_tensor_error(_ns_a, _ns_b):
         with pytest.raises(NotImplementedError):
             nps.matmul(_ns_a, _ns_b)
 
     ns_a = nps.array([[[0, 1], [2, 3]], [[4, 5], [6, 7]]])
     ns_b = nps.array([[[7, 6], [5, 4]], [[3, 2], [1, 0]]])
-    check_matmul_tensor(ns_a, ns_b)
+    test_matmul_tensor_error(ns_a, ns_b)
 
 
 def test_inner_outer(nps_app_inst):
@@ -150,7 +150,7 @@ def test_broadcast(nps_app_inst):
     import pytest
     assert nps_app_inst is not None
 
-    def check_matrix_broadcast_mismatch_simple(_np_a, _np_b):
+    def check_broadcast_mismatch_simple(_np_a, _np_b):
         _ops = ['add', 'subtract']
 
         for _op in _ops:
@@ -162,11 +162,11 @@ def test_broadcast(nps_app_inst):
             with pytest.raises(ValueError):
                 np_op(_np_a, _np_b)
             with pytest.raises(ValueError):
-                # TODO(bcp): Add synchronous broadcast checks for simple operands.
+                # TODO (bcp): Add synchronous broadcast checks for simple operands.
                 res = ns_op(_ns_a, _ns_b)
                 res.touch()
 
-    def check_matrix_broadcast_mismatch_tensor(_np_a, _np_b, axes):
+    def check_broadcast_mismatch_tensor(_np_a, _np_b, axes):
         _ns_a = nps.array(_np_a)
         _ns_b = nps.array(_np_b)
 
@@ -177,15 +177,24 @@ def test_broadcast(nps_app_inst):
 
     np_A = np.random.randn(2, 4)
     np_B = np.random.randn(2, 2)
-    check_matrix_broadcast_mismatch_simple(np_A, np_B)
+    check_broadcast_mismatch_simple(np_A, np_B)
 
     np_A = np.random.randn(2, 1)
     np_B = np.random.randn(2, 1)
-    check_matrix_broadcast_mismatch_tensor(np_A, np_B, 1)
+    check_broadcast_mismatch_tensor(np_A, np_B, 1)
 
     np_A = np.random.randn(2, 2)
     np_B = np.random.randn(2, 1)
-    check_matrix_broadcast_mismatch_tensor(np_A, np_B, 2)
+    check_broadcast_mismatch_tensor(np_A, np_B, 2)
+
+    np_A = np.random.randn(2, 2, 3)
+    np_B = np.random.randn(2, 2, 2)
+    check_broadcast_mismatch_tensor(np_A, np_B, 3)
+
+    np_A = np.random.randn(2, 2, 3)
+    np_B = np.random.randn(2, 2, 2)
+    check_broadcast_mismatch_tensor(np_A, np_B, 2)
+
 
 if __name__ == "__main__":
     from nums.core import application_manager