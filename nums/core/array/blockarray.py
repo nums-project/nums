f1c66b4b75e92e8daadd02898d5aa6b74710688b
Balaji Veeramani <bveeramani@berkeley.edu>
2021-04-25 19:01:17 -0700

    Update blockarray.py

--- nums/core/array/blockarray.py
+++ nums/core/array/blockarray.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO: Refactor this module so that it contains fewer lines.
+# pylint: disable=too-many-lines
 
 import itertools
 
