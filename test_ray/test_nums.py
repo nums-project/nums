73f74a145aa5c6ecfc0334f6692dde8a39ddc084
Anthony <ant1ng@berkeley.edu>
2021-04-27 22:37:55 -0700

    Added ants tests for multi-node experiments

--- test_ray/test_nums.py
+++ test_ray/test_nums.py
@@ -3,6 +3,7 @@ import argparse
 import ray
 import nums.numpy as nps
 import nums
+import numpy as np
 from nums.core import settings
 
 
@@ -22,17 +23,19 @@ def main(address, work_dir, use_head, cluster_shape):
     # So this will be 800MB object.
     x1 = nps.random.randn(size, size)
     x2 = nps.random.randn(size, size)
+    
     result = x1 @ x2
-    print(result.touch())
+    # print(result.touch())
+    print("Initial size:", size)
+    print("Result shape:", result.shape)
     print("writing result")
-    # write_result = nums.write(work_dir + "/result", result)
-    # write_result.get()
-
+    write_result = nums.write(work_dir + "/result", result)
+    print("Print True: ", (x1.get() @ x2.get() == result.get()).any())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--address', default="")
-    parser.add_argument('--work-dir', default="/global/homes/e/elibol/dev/test_ray")
+    parser.add_argument('--work-dir', default="/global/homes/a/ant1ng/nums/test_ray")
     parser.add_argument('--use-head', action="store_true", help="")
     parser.add_argument('--cluster-shape', default="1,1")
     args = parser.parse_args()
