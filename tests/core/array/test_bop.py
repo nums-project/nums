3d91b0a44f7c7c5adefc49ee6ada56823c40703c
Melih Elibol <elibol@gmail.com>
2021-05-27 01:04:24 -0700

    add some bops.

--- tests/core/array/test_bop.py
+++ tests/core/array/test_bop.py
@@ -26,59 +26,59 @@ from nums.core.array.application import ArrayApplication
 from nums.core.storage.storage import BimodalGaussian
 
 
-# def test_matmul(app_inst: ArrayApplication):
-#     real_X, _ = BimodalGaussian.get_dataset(100, 9)
-#     X = app_inst.array(real_X, block_shape=(100, 1))
-#     X_sqr = X.T @ X
-#     assert np.allclose(X_sqr.get(), real_X.T @ real_X)
-#
-#
-# def test_matvec(app_inst: ArrayApplication):
-#     X = app_inst.array(np.arange(200).reshape(2, 100), block_shape=(1, 10))
-#     y1 = app_inst.array(np.arange(100).reshape(100, 1), block_shape=(10, 1))
-#     assert np.allclose((X @ y1).get(), X.get() @ y1.get())
-#     y2 = app_inst.array(np.arange(100).reshape(100), block_shape=(10,))
-#     assert np.allclose((X @ y2).get(), X.get() @ y2.get())
-#     # This won't trigger optimized routine, but it is rare and strange so not worth addressing.
-#     # TODO (hme): Apparently this is invalid. Figure out why.
-#     # y3 = app_inst.array(np.arange(100).reshape((100, 1, 1)), block_shape=(10, 1, 1))
-#     # assert np.allclose((X @ y3).get(), X.get() @ y3.get())
-#
-#
-# def test_vecdot(app_inst: ArrayApplication):
-#     size = 9
-#     block_size = 3
-#     y1 = app_inst.array(np.arange(size).reshape(size, 1), block_shape=(block_size, 1))
-#     y2 = app_inst.array(np.arange(size).reshape(size, 1), block_shape=(block_size, 1))
-#     assert np.allclose((y1.T @ y2).get(), y1.T.get() @ y2.get())
-#     y1 = app_inst.array(np.arange(size).reshape(size), block_shape=(block_size,))
-#     y2 = app_inst.array(np.arange(size).reshape(size), block_shape=(block_size,))
-#     assert np.allclose((y1.T @ y2).get(), y1.T.get() @ y2.get())
-#     y1 = app_inst.array(np.arange(size).reshape(size), block_shape=(block_size,))
-#     y2 = app_inst.array(np.arange(size).reshape(size, 1), block_shape=(block_size, 1))
-#     assert np.allclose((y1.T @ y2).get(), y1.T.get() @ y2.get())
-#     assert np.allclose((y2.T @ y1).get(), y2.T.get() @ y1.get())
-#     y1 = app_inst.array(np.arange(size).reshape(1, size), block_shape=(1, block_size))
-#     y2 = app_inst.array(np.arange(size).reshape(size, 1), block_shape=(block_size, 1))
-#     assert np.allclose((y1 @ y2).get(), y1.get() @ y2.get())
-#     y1 = app_inst.array(np.arange(size).reshape(1, size), block_shape=(1, block_size))
-#     y2 = app_inst.array(np.arange(size).reshape(1, size), block_shape=(1, block_size))
-#     assert np.allclose((y1 @ y2.T).get(), y1.get() @ y2.T.get())
-#
-#
-# def test_tensordot_basic(app_inst: ArrayApplication):
-#     shape = 2, 4, 10, 15
-#     npX = np.arange(np.product(shape)).reshape(*shape)
-#     rX = app_inst.array(npX, block_shape=(1, 2, 10, 3))
-#
-#     rResult = rX.T.tensordot(rX, axes=1)
-#     assert np.allclose(
-#         rResult.get(),
-#         (np.tensordot(npX.T, npX, axes=1))
-#     )
-#     common.check_block_integrity(rResult)
-#
-#
+def test_matmul(app_inst: ArrayApplication):
+    real_X, _ = BimodalGaussian.get_dataset(100, 9)
+    X = app_inst.array(real_X, block_shape=(100, 1))
+    X_sqr = X.T @ X
+    assert np.allclose(X_sqr.get(), real_X.T @ real_X)
+
+
+def test_matvec(app_inst: ArrayApplication):
+    X = app_inst.array(np.arange(200).reshape(2, 100), block_shape=(1, 10))
+    y1 = app_inst.array(np.arange(100).reshape(100, 1), block_shape=(10, 1))
+    assert np.allclose((X @ y1).get(), X.get() @ y1.get())
+    y2 = app_inst.array(np.arange(100).reshape(100), block_shape=(10,))
+    assert np.allclose((X @ y2).get(), X.get() @ y2.get())
+    # This won't trigger optimized routine, but it is rare and strange so not worth addressing.
+    # TODO (hme): Apparently this is invalid. Figure out why.
+    # y3 = app_inst.array(np.arange(100).reshape((100, 1, 1)), block_shape=(10, 1, 1))
+    # assert np.allclose((X @ y3).get(), X.get() @ y3.get())
+
+
+def test_vecdot(app_inst: ArrayApplication):
+    size = 9
+    block_size = 3
+    y1 = app_inst.array(np.arange(size).reshape(size, 1), block_shape=(block_size, 1))
+    y2 = app_inst.array(np.arange(size).reshape(size, 1), block_shape=(block_size, 1))
+    assert np.allclose((y1.T @ y2).get(), y1.T.get() @ y2.get())
+    y1 = app_inst.array(np.arange(size).reshape(size), block_shape=(block_size,))
+    y2 = app_inst.array(np.arange(size).reshape(size), block_shape=(block_size,))
+    assert np.allclose((y1.T @ y2).get(), y1.T.get() @ y2.get())
+    y1 = app_inst.array(np.arange(size).reshape(size), block_shape=(block_size,))
+    y2 = app_inst.array(np.arange(size).reshape(size, 1), block_shape=(block_size, 1))
+    assert np.allclose((y1.T @ y2).get(), y1.T.get() @ y2.get())
+    assert np.allclose((y2.T @ y1).get(), y2.T.get() @ y1.get())
+    y1 = app_inst.array(np.arange(size).reshape(1, size), block_shape=(1, block_size))
+    y2 = app_inst.array(np.arange(size).reshape(size, 1), block_shape=(block_size, 1))
+    assert np.allclose((y1 @ y2).get(), y1.get() @ y2.get())
+    y1 = app_inst.array(np.arange(size).reshape(1, size), block_shape=(1, block_size))
+    y2 = app_inst.array(np.arange(size).reshape(1, size), block_shape=(1, block_size))
+    assert np.allclose((y1 @ y2.T).get(), y1.get() @ y2.T.get())
+
+
+def test_tensordot_basic(app_inst: ArrayApplication):
+    shape = 2, 4, 10, 15
+    npX = np.arange(np.product(shape)).reshape(*shape)
+    rX = app_inst.array(npX, block_shape=(1, 2, 10, 3))
+
+    rResult = rX.T.tensordot(rX, axes=1)
+    assert np.allclose(
+        rResult.get(),
+        (np.tensordot(npX.T, npX, axes=1))
+    )
+    common.check_block_integrity(rResult)
+
+
 # def test_tensordot_large_shape(app_inst: ArrayApplication):
 #     a = np.arange(4 * 6 * 10 * 90).reshape((90, 10, 6, 4))
 #     b = np.arange(4 * 6 * 10 * 75).reshape((4, 6, 10, 75))
