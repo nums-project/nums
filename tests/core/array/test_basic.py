de09f920eb8afadae61a80e5e0b2233885c68fd3
Melih Elibol <elibol@gmail.com>
2021-06-01 23:05:07 -0700

    fix linting.

--- tests/core/array/test_basic.py
+++ tests/core/array/test_basic.py
@@ -14,9 +14,6 @@
 # limitations under the License.
 
 
-# pylint: disable=wrong-import-order
-import common
-
 import numpy as np
 import pytest
 
@@ -26,6 +23,8 @@ from nums.core.grid.grid import ArrayGrid
 from nums.core.grid.grid import DeviceID
 from nums.core.storage.storage import BimodalGaussian
 from nums.core.systems import utils as systems_utils
+
+import common  # pylint: disable=import-error, wrong-import-order
 
 
 def test_scalar_op(app_inst: ArrayApplication):
