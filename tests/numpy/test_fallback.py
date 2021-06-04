573e4bbce6731e8321561dcbf7e382676b2f489b
nums-project <66851991+nums-project@users.noreply.github.com>
2021-06-03 22:46:19 -0700

    enable untested fallbacks. (#62)
    Co-authored-by: Melih Elibol <elibol@gmail.com>

--- tests/numpy/test_fallback.py
+++ tests/numpy/test_fallback.py
@@ -30,7 +30,6 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 def test_doctest_fallback(nps_app_inst):
     import nums.numpy as nps
-    import numpy as np
     assert nps_app_inst is not None
 
     passed = []
@@ -39,7 +38,7 @@ def test_doctest_fallback(nps_app_inst):
 
     plot_funcs = {"kaiser", "bartlett", "hanning", "blackman", "histogram2d", "interp", "sinc"}
 
-    for func in settings.doctest_fallback:
+    for func in settings.doctest_fallbacks:
         nps_func = nps.__getattribute__(func)
         nps_func.__doc__ = update_doc_string(np.__getattribute__(func).__doc__)
 
@@ -81,11 +80,196 @@ def test_manual_cov(nps_app_inst):
     assert np.allclose(np.cov(x), np.array(11.71))
 
 
+def test_manual(nps_app_inst):
+    assert nps_app_inst is not None
+    import nums.numpy as nps
+
+    test_set = {}
+    test_set.update(get_test_set_a())
+    test_set.update(get_test_set_b2f())
+    test_set.update(get_test_set_g2n())
+    test_set.update(get_test_set_o2z())
+
+    for func_name in test_set:
+        nps_func = nps.__getattribute__(func_name)
+        np_func = np.__getattribute__(func_name)
+        for params in test_set[func_name]:
+            assert np.allclose(nps_func(*params).get(),
+                               np_func(*params))
+
+
+def get_test_set_a():
+    return {
+        'angle': [
+            ([1.0, 1.0j, 1 + 1j],),
+            (1 + 1j, True)
+        ],
+        'append': [
+            ([1, 2, 3], [[4, 5, 6], [7, 8, 9]]),
+            ([[1, 2, 3], [4, 5, 6]], [[7, 8, 9]], 0)
+        ],
+        'argsort': [
+            (np.array([3, 1, 2]),),
+            (np.array([[0, 3], [2, 2]]), 0),
+            (np.array([[0, 3], [2, 2]]), 1)
+        ],
+        'around': [(56294995342131.5, 3)],
+        'apply_along_axis': [
+            (lambda a: (a[0] + a[-1]) * 0.5, 0, np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+        ],
+        'apply_over_axes': [
+            (np.sum, np.arange(24).reshape(2, 3, 4), [0, 2])
+        ],
+        'array_split': [],
+        'argpartition': [],
+        'asarray': [],
+        'asarray_chkfinite': [],
+        'average': []
+    }
+
+
+def get_test_set_b2f():
+    return {
+        'bartlett': [(12,)],
+        'bincount': [],
+        'blackman': [],
+        'choose': [],
+        'common_type': [],
+        'correlate': [],
+        'count_nonzero': [],
+        'cov': [],
+        'cross': [],
+        'delete': [],
+        'diag_indices': [],
+        'diagonal': [],
+        'diff': [],
+        'digitize': [],
+        'divmod': [],
+        'dot': [],
+        'dsplit': [],
+        'ediff1d': [],
+        'einsum': [],
+        'einsum_path': [],
+        'extract': [],
+        'fill_diagonal': [],
+        'flatnonzero': [],
+        'flip': [],
+        'fliplr': [],
+        'flipud': [],
+        'frexp': [],
+        'frombuffer': [],
+        'fromfile': [],
+        'fromfunction': [],
+        'frompyfunc': [],
+        'full_like': []
+    }
+
+
+def get_test_set_g2n():
+    return {
+        'geomspace': [],
+        'gradient': [],
+        'hamming': [],
+        'hanning': [],
+        'histogram': [],
+        'histogram2d': [],
+        'histogram_bin_edges': [],
+        'histogramdd': [],
+        'hsplit': [],
+        'i0': [],
+        'imag': [],
+        'in1d': [],
+        'indices': [],
+        'insert': [],
+        'interp': [],
+        'intersect1d': [],
+        'isclose': [],
+        'iscomplex': [],
+        'iscomplexobj': [],
+        'isin': [],
+        'isneginf': [],
+        'isposinf': [],
+        'isreal': [],
+        'isrealobj': [],
+        'isscalar': [],
+        'ix_': [],
+        'kaiser': [(12, 14)],
+        'kron': [],
+        'lexsort': [],
+        'maximum_sctype': [],
+        'median': [],
+        'meshgrid': [],
+        'min_scalar_type': [],
+        'mintypecode': [],
+        'modf': [],
+        'moveaxis': [],
+        'nan_to_num': [],
+        'nanargmax': [],
+        'nanargmin': [],
+        'nanmedian': [],
+        'nanpercentile': [],
+        'nanquantile': [],
+        'nonzero': []
+    }
+
+
+def get_test_set_o2z():
+    return {
+        'obj2sctype': [],
+        'packbits': [],
+        'pad': [],
+        'percentile': [],
+        'piecewise': [],
+        'place': [],
+        'poly': [],
+        'polyadd': [],
+        'polyder': [],
+        'polydiv': [],
+        'polyfit': [],
+        'polyint': [],
+        'polymul': [],
+        'polyval': [],
+        'prod': [],
+        'promote_types': [],
+        'ptp': [],
+        'put': [],
+        'put_along_axis': [],
+        'putmask': [],
+        'quantile': [],
+        'ravel': [],
+        'real': [],
+        'real_if_close': [],
+        'require': [],
+        'result_type': [],
+        'rollaxis': [],
+        'sctype2char': [],
+        'select': [],
+        'sinc': [],
+        'sort': [],
+        'stack': [],
+        'take': [],
+        'take_along_axis': [],
+        'trace': [],
+        'tril_indices': [],
+        'trim_zeros': [],
+        'triu_indices': [],
+        'unique': [],
+        'unpackbits': [],
+        'unravel_index': [],
+        'unwrap': [],
+        'vander': [],
+        'vdot': [],
+        'vsplit': [],
+        'who': []
+    }
+
+
 if __name__ == "__main__":
     # pylint: disable=import-error
     from nums.core import application_manager
 
     settings.system_name = "serial"
     nps_app_inst = application_manager.instance()
-    test_doctest_fallback(nps_app_inst)
-    test_manual_cov(nps_app_inst)
+    # test_doctest_fallback(nps_app_inst)
+    # test_manual_cov(nps_app_inst)
+    test_manual(nps_app_inst)
