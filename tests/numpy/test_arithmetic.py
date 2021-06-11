e5c1b957ad20dbffe1276db921e96259932785b8
Brian Park <briancpark@berkeley.edu>
2021-06-11 12:23:50 -0400

    reformatted tests with black

--- tests/numpy/test_arithmetic.py
+++ tests/numpy/test_arithmetic.py
@@ -136,10 +136,11 @@ def test_inner_outer(nps_app_inst):
 
 # TODO (hme): Add broadcast tests.
 
+
 def test_broadcast_block_shape_error(nps_app_inst):
     assert nps_app_inst is not None
 
-    _ops = ['add', 'subtract', 'divide']
+    _ops = ["add", "subtract", "divide"]
 
     def check_value_error(_ns_a, _ns_b, _a_blockshape=None, _b_blockshape=None):
         for _op in _ops:
@@ -168,6 +169,7 @@ def test_broadcast_block_shape_error(nps_app_inst):
     nps_A = nps.random.randn(20, 20, 20)
     nps_B = nps.random.randn(20, 20, 20)
     check_value_error(nps_A, nps_B, _a_blockshape=(10, 10, 10), _b_blockshape=(2, 2, 2))
+
 
 def test_tensordot_shape_error(nps_app_inst):
     assert nps_app_inst is not None
