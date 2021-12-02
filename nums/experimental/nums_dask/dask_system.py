bad7ef7f7cb2f65697efdb5ab8e348ac4ef935a0
Melih Elibol <elibol@gmail.com>
2021-12-01 22:55:16 -0800

    shorten stack?

--- nums/experimental/nums_dask/dask_system.py
+++ nums/experimental/nums_dask/dask_system.py
@@ -163,16 +163,13 @@ class DaskSystem(SystemInterface):
 
     def call(self, name: str, args, kwargs, device_id: DeviceID, options: Dict):
         assert device_id is not None
-        workers = self._node_to_worker[device_id.node_addr]["workers"]
-        worker_addr = [workers[device_id.device_id]]
-
         func, nout = self._parse_call(name, options)
         if nout is None:
-            return self._client.submit(func, *args, **kwargs, workers=worker_addr, pure=False)
+            return self._client.submit(func, *args, **kwargs, workers=self._node_to_worker[device_id.node_addr]["workers"][device_id.device_id], pure=False)
         else:
             dfunc = dask.delayed(func, nout=nout)
             result = tuple(dfunc(*args, **kwargs))
-            return self._client.compute(result, workers=worker_addr, optimize_graph=False)
+            return self._client.compute(result, workers=self._node_to_worker[device_id.node_addr]["workers"][device_id.device_id], optimize_graph=False)
 
     def num_cores_total(self) -> int:
         return len(self._worker_addresses)
