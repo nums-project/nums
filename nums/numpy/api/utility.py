162c8ee660b177e3a95c21b45b632325f24e2865
Brian Park <briancpark@berkeley.edu>
2021-12-22 05:29:54 -0500

    pylint

--- nums/numpy/api/utility.py
+++ nums/numpy/api/utility.py
@@ -13,9 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable = redefined-builtin, too-many-lines, W1401, W0401, W0614
+
 from nums.core.application_manager import instance as _instance
 from nums.core.array.blockarray import BlockArray
-from nums.numpy import numpy_utils
 
 ############################################
 # Utility Ops
