dff506d7df10557c780143ce4c230e59ebcc22ba
Brian Park <briancpark@berkeley.edu>
2022-02-09 14:56:40 -0800

    refactor everything in creation.py

--- nums/numpy/api/algebra.py
+++ nums/numpy/api/algebra.py
@@ -297,3 +297,77 @@ def dot(a: BlockArray, b: BlockArray, out=None) -> BlockArray:
         raise NotImplementedError(
             "The dot operation on arbitrary arrays is not yet supported."
         )
+
+
+def trace(a: BlockArray, offset=0, axis1=0, axis2=1, dtype=None, out=None):
+    """Return the sum along diagonals of the array.
+
+    This docstring was copied from numpy.trace.
+
+    Some inconsistencies with the NumS version may exist.
+
+    If `a` is 2-D, the sum along its diagonal with the given offset
+    is returned, i.e., the sum of elements ``a[i,i+offset]`` for all i.
+
+    If `a` has more than two dimensions, then the axes specified by axis1 and
+    axis2 are used to determine the 2-D sub-arrays whose traces are returned.
+    The shape of the resulting array is the same as that of `a` with `axis1`
+    and `axis2` removed.
+
+    Parameters
+    ----------
+    a : BlockArray
+        Input array, from which the diagonals are taken.
+    offset : int, optional
+        Offset of the diagonal from the main diagonal. Can be both positive
+        and negative. Defaults to 0.
+    axis1, axis2 : int, optional
+        Axes to be used as the first and second axis of the 2-D sub-arrays
+        from which the diagonals should be taken. Defaults are the first two
+        axes of `a`.
+    dtype : dtype, optional
+        Determines the data-type of the returned array and of the accumulator
+        where the elements are summed. If dtype has the value None and `a` is
+        of integer type of precision less than the default integer
+        precision, then the default integer precision is used. Otherwise,
+        the precision is the same as that of `a`.
+    out : BlockArray, optional
+        Array into which the output is placed. Its type is preserved and
+        it must be of the right shape to hold the output.
+
+    Returns
+    -------
+    sum_along_diagonals : BlockArray
+        If `a` is 2-D, the sum along the diagonal is returned.  If `a` has
+        larger dimensions, then an array of sums along diagonals is returned.
+
+    See Also
+    --------
+    diag, diagonal
+
+    Notes
+    -----
+    offset != 0 is currently not supported.
+
+    out is currently not supported.
+
+    axis1 != 0 or axis2 != 1 is currently not supported.
+
+    Examples
+    --------
+    The doctests shown below are copied from NumPy.
+    They won’t show the correct result until you operate ``get()``.
+
+    >>> nps.trace(nps.eye(3)).get()  # doctest: +SKIP
+    array(3.)
+    >>> a = nps.arange(8).reshape((2,2,2))  # doctest: +SKIP
+    """
+    if offset != 0:
+        raise NotImplementedError("offset != 0 is currently not supported.")
+    if out is not None:
+        raise NotImplementedError("out is currently not supported.")
+    if axis1 != 0 or axis2 != 1:
+        raise NotImplementedError(
+            " axis1 != 0 or axis2 != 1 is currently not supported."
+        )
+    return sum(diag(a, offset), dtype=dtype, out=out)
