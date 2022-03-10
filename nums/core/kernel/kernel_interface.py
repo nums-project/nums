eb00999df81dfbbef0af0f4d8cc0fa366f4657fe
Balaji Veeramani <bveeramani@berkeley.edu>
2022-03-10 13:45:27 -0800

    Make `Kernel` an ABC

--- nums/core/kernel/kernel_interface.py
+++ nums/core/kernel/kernel_interface.py
@@ -1,44 +1,55 @@
+import abc
 from typing import Tuple, Dict
 
 from nums.core.backends.utils import method_meta
 
 
-class Kernel:
+class Kernel(abc.ABC):
+    @abc.abstractmethod
     def touch(self, arr, syskwargs: Dict):
         """
         "Touch" the given array. This returns nothing and can be used to wait for
         a computation without pulling data to the head node.
         """
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def new_block(
         self, op_name: str, grid_entry: Tuple, grid_meta: Dict, syskwargs: Dict
     ):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def random_block(
         self, rng_params, rfunc_name, rfunc_args, shape, dtype, syskwargs: Dict
     ):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def permutation(self, rng_params, size, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def diag(self, arr, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def arange(self, start, stop, step, dtype, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def sum_reduce(self, *arrs, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def transpose(self, arr, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def swapaxes(self, arr, axis1, axis2, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def create_block(
         self,
         *src_arrs,
@@ -48,14 +59,17 @@ class Kernel:
         dst_shape_bc,
         syskwargs: Dict
     ):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def update_block(self, dst_arr, *src_arrs, src_params, dst_params, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def update_block_by_index(self, dst_arr, src_arr, index_pairs, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def advanced_assign_block_along_axis(
         self,
         dst_arr,
@@ -66,8 +80,9 @@ class Kernel:
         src_coord,
         syskwargs: Dict,
     ):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def advanced_select_block_along_axis(
         self,
         dst_arr,
@@ -79,85 +94,109 @@ class Kernel:
         src_coord,
         syskwargs: Dict,
     ):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def bop(self, op, a1, a2, a1_T, a2_T, axes, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def bop_reduce(self, op, a1, a2, a1_T, a2_T, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def split(self, arr, indices_or_sections, axis, transposed, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def shape_dtype(self, arr, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def size(self, arr, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def tdigest_chunk(self, arr, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def percentiles_from_tdigest(self, q, *digests, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def select_median(self, arr, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def weighted_median(self, *arr_and_weights, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def pivot_partition(self, arr, pivot, op, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     @method_meta(num_returns=2)
     def qr(self, *arrays, mode="reduced", axis=None, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def cholesky(self, arr, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     @method_meta(num_returns=3)
     def svd(self, arr, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def inv(self, arr, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def array_compare(self, func_name, a, b, args, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def map_uop(self, op_name, arr, args, kwargs, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def where(self, arr, x, y, block_slice_tuples, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def reduce_axis(self, op_name, arr, axis, keepdims, transposed, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
     # Scipy
 
+    @abc.abstractmethod
     def xlogy(self, arr_x, arr_y, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def logical_and(self, *bool_list, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def astype(self, arr, dtype_str, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def arg_op(
         self, op_name, arr, block_slice, other_argoptima, other_optima, syskwargs: Dict
     ):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def reshape(self, arr, shape, syskwargs: Dict):
-        raise NotImplementedError()
+        pass
 
+    @abc.abstractmethod
     def identity(self, value):
-        raise NotImplementedError()
+        pass
 
 
 class KernelImp:
